948dd45af293405b25f0204e6924ef57af59d460
hirfunaki <72174111+hirfunaki@users.noreply.github.com>
2023-08-02 12:53:24 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+function main() {
+    const canvas = document.getElementById("renderCanvas");
+  
+    const engine = new BABYLON.Engine(canvas);
+  
+    function createScene() {
+  
+      // 新しいシーンオブジェクトを作成
+      const scene = new BABYLON.Scene(engine);
+  
+      // カメラを追加
+      const camera = new BABYLON.ArcRotateCamera("Camera", 0, 0.8, 10, BABYLON.Vector3.Zero(), scene);
+      camera.attachControl(canvas, false);
+  
+      // 照明を追加
+      const light = new BABYLON.HemisphericLight();
+  
+      // GLBファイルの読み込む
+      BABYLON.SceneLoader.Append("https://ft-lab.github.io/gltf/apple/", "apple.glb", scene, 
+        function (newMeshes) {
+        
+          const mesh = scene.meshes[0];
+  
+          scene.activeCamera = null;
+          scene.createDefaultCameraOrLight(true);
+          scene.activeCamera.attachControl(canvas, false);
+        },
+      );
+  
+      return scene;
+    }
+  
+    const scene = createScene();
+  
+    engine.runRenderLoop(() => {
+      scene.render();
+    });
+  
+    window.addEventListener("resize", () => {
+      engine.resize();
+    });
+  }
+  
+  window.addEventListener("DOMContentLoaded", main);
